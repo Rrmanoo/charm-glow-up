cb243a2dfd3fc42b76515e74950fefa51b351255
Rrmanoo <armannurken@users.noreply.github.com>
2026-04-08 10:48:00 +0500

    Fix GitHub Pages SPA routing with basename and 404 redirect

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/charm-glow-up">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
